01b8b35ee4db87e1adf6ed649ec2e467369a12d2
Jordan Gonzalez <jordan.olaves91@gmail.com>
2023-06-05 22:18:55 -0500

    first commit

---
+++ src/main/java/org/exerciseQAAPP/configuration/mobile/MobileDriverManager.java
@@ -0,0 +1,10 @@
+package org.exerciseQAAPP.configuration.mobile;
+
+public abstract class MobileDriverManager {
+
+    /**
+     * The driver
+     */
+    protected final AndroidDriver<AndroidElement> driver;
+
+}
